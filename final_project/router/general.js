1dbe1e674c7d5c222ff88434e51d1642fbc651e6
GopinathSittampalam <sittampalamgopinath@gmail.com>
2023-05-17 12:18:22 +0530

    Task 5 completed

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,32 +12,45 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //res.send(JSON.stringify(books));
+  //return res.status(200).json(JSON.stringify({books},null,0));
+  return res.status(200).json({books});
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let myISBN = req.params.isbn;
+  let myBook = books.filter((book)=>book.isbn === myISBN);
+  //res.send(JSON.stringify(myBook));
+  return res.status(200).json(myBook);
+  //return res.status(200).json({message: "Books were filtered by ISBN"});
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let myAuthor = req.params.author;
+  let myBook = books.filter((book)=>book.author === myAuthor);
+  //res.send(JSON.stringify(myBook));
+  return res.status(200).json(myBook);
+  //return res.status(200).json({message: "Books were filtered by author",body:{myBook}});
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let myTitle = req.params.title;
+  let myBook = books.filter((book)=>book.title === myTitle);
+  //res.send(JSON.stringify(myBook));
+  return res.status(200).json(myBook);
+  //return res.status(200).json({message: "Books were filtered by title"});
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let myISBN = req.params.isbn;
+  let review = books.filter((book)=>book.isbn === myISBN);
+  //res.send(JSON.stringify(review));
+  return res.status(200).json(review);
+  //return res.status(200).json({message: "Reviews were filtered by ISBN"});
 });
 
 module.exports.general = public_users;
