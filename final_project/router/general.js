9f60504a4c54bacfb4c5f4da5f64ed79d616e339
GopinathSittampalam <sittampalamgopinath@gmail.com>
2023-05-18 12:16:43 +0530

    all tasks completed

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,7 +3,7 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
-
+const axios = require('axios').default;
 
 public_users.post("/register", (req,res) => {
   users.push(req.body);
@@ -12,8 +12,6 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //res.send(JSON.stringify(books));
-  //return res.status(200).json(JSON.stringify({books},null,0));
   return res.status(200).json({books});
 });
 
@@ -21,27 +19,21 @@ public_users.get('/',function (req, res) {
 public_users.get('/isbn/:isbn',function (req, res) {
   let myISBN = req.params.isbn;
   let myBook = books.filter((book)=>book.isbn === myISBN);
-  //res.send(JSON.stringify(myBook));
   return res.status(200).json(myBook);
-  //return res.status(200).json({message: "Books were filtered by ISBN"});
- });
-  
+});
+
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   let myAuthor = req.params.author;
   let myBook = books.filter((book)=>book.author === myAuthor);
-  //res.send(JSON.stringify(myBook));
   return res.status(200).json(myBook);
-  //return res.status(200).json({message: "Books were filtered by author",body:{myBook}});
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   let myTitle = req.params.title;
   let myBook = books.filter((book)=>book.title === myTitle);
-  //res.send(JSON.stringify(myBook));
   return res.status(200).json(myBook);
-  //return res.status(200).json({message: "Books were filtered by title"});
 });
 
 //  Get book review
